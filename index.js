6fe7fb4b110a9fe4fef7b4069c0965350899e44e
Tobias Allrich <tobias@arbeit>
2023-12-11 15:25:11 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const fs = require('fs');
+const bodyParser = require('body-parser');
+const uuid = require('uuid');
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+app.use('/tasks', express.static('tasks')); // Serve static files from the 'tasks' folder
+
+// Site 1: Text Editor
+app.post('/saveTask', (req, res) => {
+  const { headline, dueDate, progress, description } = req.body;
+  const taskId = uuid.v4();
+  const task = { taskId, headline, dueDate, progress, description };
+  fs.writeFileSync(`tasks/${taskId}.json`, JSON.stringify(task));
+  res.send('Task saved successfully!');
+});
+
+app.put('/updateTask/:taskId', (req, res) => {
+  const { taskId } = req.params;
+  const { headline, dueDate, progress, description } = req.body;
+  const task = { taskId, headline, dueDate, progress, description };
+  fs.writeFileSync(`tasks/${taskId}.json`, JSON.stringify(task));
+  res.send('Task updated successfully!');
+});
+
+app.get('/loadTasks', (req, res) => {
+  const files = fs.readdirSync('./tasks');
+  const tasks = files.filter(file => file.endsWith('.json')).map(file => {
+    const data = fs.readFileSync(`tasks/${file}`, 'utf-8');
+    return JSON.parse(data);
+  });
+  res.json(tasks);
+});
+
+// Site 2: Front End
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/editor', (req, res) => {
+  res.sendFile(__dirname + '/editor.html');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
